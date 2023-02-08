f35588529677d00949918dcc2b38a8b49bf0e383
Vincent <VincentNSWE@gmail.com>
2023-02-08 09:55:30 +0800

    initial_commit

---
+++ src/test/java/ssf/day13_lecture/Day13LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package ssf.day13_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day13LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
